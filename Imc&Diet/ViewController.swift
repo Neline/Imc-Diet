47d54571a765d09a7bfb9b1e18d3de1ed5c663ba
Admin <admin@anneline.local>
2019-01-10 10:44:34 +0100

    Initial Commit

---
+++ Imc&Diet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Imc&Diet
+//
+//  Created by Admin on 10/01/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
